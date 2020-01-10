4b761c65ac961cd20f2538658646c492b74d96a1
Wassim Vitols <eywassim@gmail.com>
2020-01-10 19:34:56 +1030

    Add cross compilation support

--- release.nix
+++ release.nix
@@ -1,16 +1,19 @@
 { nixpkgs ? <nixpkgs>,
-  supportedSystems ? [ "aarch64-linux" ],
   ethaddr ? null
 }:
 
 let
+  targetSystem = "aarch64-linux";
+
   sdImageConfig = import (nixpkgs+"/nixos/lib/eval-config.nix") {
      modules = [ ./modules/sd-image.nix ];
   };
 
-  mkJob  =
-    { system ? builtins.currentSystem, select }:
-	select (import nixpkgs { inherit system; });
+  mkJob  = { select, system ? builtins.currentSystem }: select (import nixpkgs (
+	if (targetSystem == system)
+        then { inherit system; }
+	else { crossSystem = targetSystem; }
+  ));
 
   jobs = rec {
 
@@ -21,12 +24,14 @@ let
 
     kernel_5_5 = mkJob { select = pkgs: (pkgs.callPackage ./packages/linux_odroid_n2/linux-5.5.nix pkgs); };
 
-    uboot = mkJob { 
-        # This cannot be build on aarch64 because the tool used to bind the propierty blobs and the built binary, aml_encrypt_g12b, is only available for the x86_64 arch
+    uboot = (
+        # This cannot be built on aarch64 because the tool used to bind the blobs
+        # and the built binary, aml_encrypt_g12b, is only available for the x86_64 arch.
         # Maybe, meson-tools' amlbootsig could be used instead
-        system = "x86_64-linux";
-	select = pkgs: (pkgs.callPackage ./packages/uboot_odroid_n2/default.nix (pkgs // { inherit ethaddr; }));
-    };
+        assert (builtins.currentSystem == "x86_64-linux");
+
+	mkJob { select = pkgs: (pkgs.callPackage ./packages/uboot_odroid_n2/default.nix (pkgs // { inherit ethaddr; })); }
+    );
 
   };
 in
