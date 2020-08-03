ee78fb83a1273f302fd547af274a96c8abaadb7a
Moritz Angermann <moritz.angermann@gmail.com>
2020-08-03 14:15:39 +0000

    Drop the `aml_encrypt_g12a` tool. Replace with `meson64-tools`.

--- packages/uboot_odroid_n2/default.nix
+++ packages/uboot_odroid_n2/default.nix
@@ -1,8 +1,5 @@
 { buildUBoot, stdenv, fetchFromGitLab, ... } @ args:
 
-let
-  extra = ./extra;
-in
 buildUBoot {
   version = "v2020.04.20";
   # See https://gitlab.denx.de/u-boot/custodians/u-boot-amlogic/-/commit/6de936b011fb02d1019a69aea0184cee4a578f59
@@ -17,15 +14,4 @@ buildUBoot {
   defconfig = "odroid-n2_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
   filesToInstall = [ "u-boot.bin" ".config" ];
-  preBuild = ''
-  cp -Rf ${extra} extra
-  chmod -Rf ug+w extra
-  patchShebangs ./extra/pack.sh
-  patchShebangs ./extra/fip/blx_fix.sh
-  '';
-  postBuild = ''
-    chmod -Rf ug+w extra
-    ./extra/pack.sh
-    cp extra/fip/u-boot.bin .
-  '';
 }
--- release.nix
+++ release.nix
@@ -27,12 +27,14 @@ let
 
     kernel_5_6 = mkJob { select = pkgs: (pkgs.callPackage ./packages/linux_odroid_n2/linux-5.6.nix pkgs); };
 
-  } // (optionalAttrs (builtins.currentSystem == "x86_64-linux") {
+  } // (optionalAttrs (builtins.currentSystem == "x86_64-linux") rec {
 
     # This cannot be built on aarch64 because the tool used to bind the blobs
     # and the built binary, aml_encrypt_g12b, is only available for the x86_64 arch.
     # Maybe, meson-tools' amlbootsig could be used instead
-    uboot =  mkJob { select = pkgs: (pkgs.callPackage ./packages/uboot_odroid_n2/default.nix pkgs); };
+    uboot = mkJob { select = pkgs: (pkgs.callPackage ./packages/uboot_odroid_n2 pkgs); };
+
+    fip = mkJob { select = pkgs: (pkgs.callPackage ./packages/fip (pkgs // { inherit uboot; })); };
 
  }) // (optionalAttrs (broken) {
 
